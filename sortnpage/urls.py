3f2961430213bb2af5a7a447d2cf94be471e6b87
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2021-03-23 14:59:22 +0530

    add datatables

--- sortnpage/urls.py
+++ sortnpage/urls.py
@@ -9,5 +9,7 @@ router.register(r'pagination-demos', PaginationDemoViewSet)
 
 urlpatterns = [
     path('pagination-demos/', views.PaginationDemoList.as_view(), name='PaginationDemoList'),
+    path('pagination-demos-datatables/', views.PaginationDemoListDatatables.as_view(), name='PaginationDemoListDatatables'),
+    path('pagination-demos-data/', views.PaginationDemoListData.as_view(), name='PaginationDemoListData'),
     path('pagination-demo/<pk>', views.PaginationDemoDetail.as_view(), name='PaginationDemoDetail'),
 ]
--- sortnpage/views.py
+++ sortnpage/views.py
@@ -1,6 +1,16 @@
-from django.views.generic import ListView, DetailView
+from functools import reduce
+from operator import and_
+
+from django.views.generic import ListView, DetailView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.http.response import JsonResponse
+from django.db.models import Q
+
 from .models import PaginationDemo
+from .serializers import PaginationDemoSerializer
+
 
 class PaginationDemoList(LoginRequiredMixin, ListView):
     model = PaginationDemo
@@ -9,19 +19,79 @@ class PaginationDemoList(LoginRequiredMixin, ListView):
     # ordering = ['name']
 
     def get_ordering(self):
-        ordering = self.request.GET.get('ordering', 'amount')
+        ordering = self.request.GET.get("ordering", "amount")
 
         # validate ordering
-        if ordering not in ('name', 'amount', '-name', '-amount', ):
-            ordering = 'amount'
-        
+        if ordering not in (
+            "name",
+            "amount",
+            "-name",
+            "-amount",
+        ):
+            ordering = "amount"
+
         return ordering
+
 
 class PaginationDemoDetail(LoginRequiredMixin, DetailView):
     model = PaginationDemo
     # login_url = '/api-auth/login/'
-    
+
     def get_object(self, queryset=None):
         pk = self.kwargs.get(self.pk_url_kwarg)
         obj = PaginationDemo.objects.get(uuId=pk)
         return obj
+
+
+class PaginationDemoListDatatables(View):
+    def get(self, request):
+        return render(request, "sortnpage/paginationdemo_list_datatables.html")
+
+
+class PaginationDemoListData(View):
+    def get(self, request):
+        # print(request.GET)
+
+        # Pagination limit and offset
+        try:
+            start = int(request.GET.get("start", 0))
+        except ValueError:
+            start = 0
+        try:
+            length = int(request.GET.get("length", 10))
+        except ValueError:
+            length = 10
+
+        columns = ["id", "name", "amount"]
+        ordering = "name"
+
+        # Order Column
+        try:
+            order_col = int(request.GET.get("order[0][column]", 1))
+        except ValueError:
+            order_col = 1
+        if order_col:
+            try:
+                ordering = columns[order_col]
+            except KeyError:
+                pass
+
+        # Order Direction
+        order_dir = request.GET.get("order[0][dir]")
+        if order_dir and order_dir == "desc":
+            ordering = "-" + ordering
+
+        allObjects = PaginationDemo.objects.all().order_by(ordering)
+
+        # Add paginator
+        paginator = Paginator(allObjects, length)
+        page = (start // length) + 1
+        pageObjects_list = paginator.get_page(page)
+
+        return JsonResponse(
+            {
+                "recordsTotal": paginator.count,
+                "recordsFiltered": paginator.count,
+                "data": PaginationDemoSerializer(pageObjects_list, many=True).data,
+            }
+        )
